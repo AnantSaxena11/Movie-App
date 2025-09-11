0a87881e4df029173b47155939c90494796c75bb
Anant Saxena <anantsaxena5454@gmail.com>
2025-09-11 23:18:01 +0530

    project started

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern'/>
+      <div className="wrapper">
+        <header>
+          <h1>FInd <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  )
+}
+
+export default App
